e88180ca56d050d7f362c43c1bb9097878e13b8b
Shadman352126 <shadman352126@gmail.com>
2023-10-15 01:54:47 +0600

    Page and router initial setup

---
+++ src/Components/Header.jsx
@@ -0,0 +1,40 @@
+import { NavLink } from "react-router-dom";
+
+const Header = () => {
+  const navLink = (
+    <>
+      <li>
+        <NavLink to={"/"}>Home</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/login"}>Login</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/register"}>Register</NavLink>
+      </li>
+    </>
+  );
+
+  return (
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            {navLink}
+          </ul>
+        </div>
+        <a className="btn btn-ghost normal-case text-xl">
+          Context-Firebase-Router
+        </a>
+      </div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{navLink}</ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./Components/Root";
+import Home from "./Components/Home";
+import Login from "./Components/Login";
+import Register from "./Components/Register";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+    ],
   },
 ]);
 
